bca03f626612a71de0dcc76ad2c3796e49bdcd43
Your Name <ratulbdk02@gmail.com>
2021-06-28 05:18:32 -0700

    Implementation Done

--- linear.py
+++ linear.py
@@ -4,15 +4,15 @@ def linear(arr, n, search):
     for L in range (0, n):
         if(arr[L] == search):
             return L
-    return -1
+    return 0
 
-arr = [10, 20, 30, 40, 60]
+arr = [10, 20, 30, 40, 60, 32]
 n = len(arr)
 search = 60
 
 index = linear(arr, n, search)
-if (index == -1):
+if (index == 0):
     print('searching not found')
 else:
-    print('searching found at index', index)
-
+    print('searching found at index =', index)
+    print('\n')
